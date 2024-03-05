d26597e61e5722bacce21e35ac5388d98b31b022
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-05 20:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	if(str.length==1) {
+		return str;
+	}
+	
+	let obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    obj[str[i]] = (obj[str[i]] || 0) + 1;
+  }
+  let minvalue = 1
+  for (let i in obj) {
+    if (obj[i] == minvalue) {
+      minvalue = obj[i];
+      return i;
+    }
+  }
+  return null;
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
